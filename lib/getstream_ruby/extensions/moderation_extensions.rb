f41f6127fa18ff4011fb8763af4c77282064c986
Viktor Apostolski <apostolskiviktor@gmail.com>
2026-02-18 13:21:55 +0100

    lint

--- lib/getstream_ruby/extensions/moderation_extensions.rb
+++ lib/getstream_ruby/extensions/moderation_extensions.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module GetStream
+
   module Generated
+
     class ModerationClient
+
       # Experimental: Check user profile
       #
       # Warning: This is an experimental feature and the API is subject to change.
@@ -19,9 +22,12 @@ module GetStream
       # @return [GetStreamRuby::StreamResponse]
       #
       # @example
-      #   client.moderation.check_user_profile('user-id', { username: 'bad_username', image: 'https://example.com/profile.jpg' })
+      #   client.moderation.check_user_profile('user-id',
+      #     { username: 'bad_username', image: 'https://example.com/profile.jpg' })
       def check_user_profile(user_id, profile)
-        raise ArgumentError, 'Either username or image must be provided' if profile[:username].nil? && profile[:image].nil?
+        if profile[:username].nil? && profile[:image].nil?
+          raise ArgumentError, 'Either username or image must be provided'
+        end
 
         moderation_payload = Models::ModerationPayload.new
         moderation_payload.texts = [profile[:username]] if profile[:username]
@@ -33,11 +39,14 @@ module GetStream
           entity_creator_id: user_id,
           moderation_payload: moderation_payload,
           config_key: 'user_profile:default',
-          options: { force_sync: true, test_mode: true }
+          options: { force_sync: true, test_mode: true },
         )
 
         check(check_request)
       end
+
     end
+
   end
+
 end
