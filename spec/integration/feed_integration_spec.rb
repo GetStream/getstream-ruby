625dbfd547dd98624eb10bd5d7fbb923234eb65e
Aditya Agarwal <itsmeadityaagarwal@gmail.com>
2026-03-20 17:45:28 +0100

    update sdk

--- spec/integration/feed_integration_spec.rb
+++ spec/integration/feed_integration_spec.rb
@@ -489,7 +489,7 @@ RSpec.describe 'Feed Integration Tests', type: :integration do
       # Add comment
       comment_request = GetStream::Generated::Models::AddCommentRequest.new(
         comment: 'This is a test comment from Ruby SDK',
-        object_id: activity_id,
+        _object_id: activity_id,
         object_type: 'activity',
         user_id: test_user_id_1,
       )
@@ -506,7 +506,7 @@ RSpec.describe 'Feed Integration Tests', type: :integration do
       # Query comments
       query_request = GetStream::Generated::Models::QueryCommentsRequest.new(
         filter: {
-          object_id: activity_id,
+          _object_id: activity_id,
         },
         limit: 10,
       )
@@ -939,7 +939,7 @@ RSpec.describe 'Feed Integration Tests', type: :integration do
 
         comment_request = GetStream::Generated::Models::AddCommentRequest.new(
           comment: comment_text,
-          object_id: post_id,
+          _object_id: post_id,
           object_type: 'activity',
           user_id: test_user_id_2,
         )
