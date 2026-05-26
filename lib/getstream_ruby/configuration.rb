b149bfaa7ab29dfe48bad0ce118863b42ee73cb5
Yun Wang <yun.wang@getstream.io>
2026-05-26 18:54:43 +0200

    feat: add max_conns_per_host, idle_timeout, connect_timeout, request_timeout, http_client to Configuration

--- lib/getstream_ruby/configuration.rb
+++ lib/getstream_ruby/configuration.rb
@@ -5,11 +5,17 @@ module GetStreamRuby
   class Configuration
 
     attr_accessor :api_key, :api_secret, :base_url, :timeout, :logger, :faraday_adapter, :faraday_adapter_options,
-                  :connection_keep_alive
+                  :connection_keep_alive, :max_conns_per_host, :idle_timeout, :connect_timeout,
+                  :request_timeout, :http_client
 
     def initialize(api_key: nil, api_secret: nil, use_env: true, **options)
       base_url = options[:base_url]
       timeout = options[:timeout]
+      request_timeout = options[:request_timeout]
+      max_conns_per_host = options[:max_conns_per_host]
+      idle_timeout = options[:idle_timeout]
+      connect_timeout = options[:connect_timeout]
+      http_client = options[:http_client]
       http_options = options[:http_options] || {}
       faraday_adapter = options[:faraday_adapter] || http_options[:faraday_adapter]
       faraday_adapter_options = options[:faraday_adapter_options] || http_options[:faraday_adapter_options]
@@ -23,15 +29,26 @@ module GetStreamRuby
         @api_key = api_key || ENV.fetch('STREAM_API_KEY', nil)
         @api_secret = api_secret || ENV.fetch('STREAM_API_SECRET', nil)
         @base_url = base_url || ENV['STREAM_BASE_URL'] || 'https://chat.stream-io-api.com'
-        @timeout = timeout || (ENV['STREAM_TIMEOUT'] || 30).to_i
+        env_request_timeout = ENV['STREAM_REQUEST_TIMEOUT'] || ENV['STREAM_TIMEOUT']
+        @request_timeout = (request_timeout || timeout || env_request_timeout || 30).to_i
+        @max_conns_per_host = (max_conns_per_host || ENV['STREAM_MAX_CONNS_PER_HOST'] || 5).to_i
+        @idle_timeout = (idle_timeout || ENV['STREAM_IDLE_TIMEOUT'] || 55).to_i
+        @connect_timeout = (connect_timeout || ENV['STREAM_CONNECT_TIMEOUT'] || 10).to_i
       else
         # Manual configuration only - no environment variables
         @api_key = api_key
         @api_secret = api_secret
         @base_url = base_url || 'https://chat.stream-io-api.com'
-        @timeout = timeout || 30
+        @request_timeout = (request_timeout || timeout || 30).to_i
+        @max_conns_per_host = (max_conns_per_host || 5).to_i
+        @idle_timeout = (idle_timeout || 55).to_i
+        @connect_timeout = (connect_timeout || 10).to_i
       end
 
+      # Keep @timeout in sync with @request_timeout for backwards compatibility.
+      @timeout = @request_timeout
+
+      @http_client = http_client
       @faraday_adapter = (faraday_adapter || ENV.fetch('STREAM_FARADAY_ADAPTER', nil))&.to_sym
       @faraday_adapter_options = faraday_adapter_options || default_adapter_options
       @connection_keep_alive = if connection_keep_alive.nil?
@@ -39,7 +56,7 @@ module GetStreamRuby
                                else
                                  connection_keep_alive
                                end
-      @logger = nil
+      @logger = options[:logger]
     end
 
     def valid?
@@ -57,9 +74,15 @@ module GetStreamRuby
         api_secret: @api_secret,
         base_url: @base_url,
         timeout: @timeout,
+        request_timeout: @request_timeout,
+        max_conns_per_host: @max_conns_per_host,
+        idle_timeout: @idle_timeout,
+        connect_timeout: @connect_timeout,
+        http_client: @http_client,
         faraday_adapter: @faraday_adapter,
         faraday_adapter_options: @faraday_adapter_options.dup,
         connection_keep_alive: @connection_keep_alive,
+        logger: @logger,
       )
     end
 
